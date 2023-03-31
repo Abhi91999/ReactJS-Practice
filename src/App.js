a2b62e7c037069e01f263a78905bdcc9bd6d8a96
AbhishekKumar <Abhishek45744@gmail.com>
2023-04-01 01:59:21 +0530

    Updated Dark mode in navbar and textbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 
+import { useState } from 'react';
 import './App.css';
+import About from './Components/About';
 import NavBar from './Components/NavBar';
 import TextForm from './Components/TextForm';
 
 function App() {
+  const [mode, setMode] = useState('light');
+  let toggleMode = () => {
+    if(mode === 'light'){
+    setMode('dark');
+    document.body.style.backgroundColor = 'ede6f6';
+    }
+    else
+    {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  }
   return (
     <>
-    <NavBar title = "TextUtils" AboutText = "About TextUtils"/>
+    <NavBar title = "TextUtils" AboutText = "About TextUtils" mode ={mode} toggleMode={toggleMode}/>
     {/* <NavBar/> */}
     <div className='container'>
-    <TextForm heading = "Enter the text to analyze below"/>
+    <TextForm heading = "Enter the text to analyze below" mode={mode}/>
+    <About/>
     </div>
     </>
   );
--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -2,15 +2,37 @@ import React, {useState} from 'react'
 
 
 export default function TextForm(props) {
-    const[text,setText] = useState('Enter the text here');
+    const[text,setText] = useState('');
+    const[text1,setText1] = useState('');
     const handleUpClick = () => {
-        console.log("handleUp is cicked" + text);
+        console.log("handleUp is clicked" + text);
         let newText = text.toUpperCase();
+        setText(newText);
+    }
+    const handleLoClick = () => {
+        console.log("handleUp is clicked" + text);
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
+    const handleClearClick = () => {
+        console.log("handleUp is clicked" + text);
+        let newText = "";
         setText(newText);
     }
     const handleOnChange = (event) => {
         console.log("On Change");
         setText(event.target.value);
+    }
+    const handleCopy = () => {
+        console.log("I am copy");
+        var text = document.getElementById("myBox");
+        text.select();
+        text.setSelectionRange(0,9999);
+        navigator.clipboard.writeText(text.value);
+    }
+    const handleExtraSpace = () => {
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "));
     }
   return (
     <>
@@ -18,14 +40,23 @@ export default function TextForm(props) {
         <h1>{props.heading}</h1>
         <div className="mb-3">
         {/* <label for="myBox" class="form-label">Example textarea</label> */}
-        <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="14"></textarea>
+        <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor : props.mode === 'light' ? 'white' : '#ede6f6'}} id="myBox" rows="14"></textarea>
         </div>
-        <button className="btn btn-pimary" onClick={handleUpClick}> Convert to upper case</button>
+        <button className="btn btn-primary mx-2" onClick={handleUpClick}> Convert to upper case</button>
+        <button className="btn btn-primary mx-2" onClick={handleLoClick}> Convert to lower case</button>
+        <button className="btn btn-primary mx-2" onClick={handleClearClick}> Clear Text</button>
+        <button className="btn btn-primary mx-2" onClick={handleCopy}> Copy</button>
+        <button className="btn btn-primary mx-2" onClick={handleExtraSpace}> Remove Extra Spaces</button>
     </div>
-    <div className='conatiner' my-3>
+    < div className='conatiner' my-3>
         <h1>Your Text Summary</h1>
         <p>{text.split(" ").length} words and {text.length} characters</p>
         <p>{0.008 * text.split(" ").length} Minutes to read</p>
+        <h2>Preview</h2>
+        <p>{text.length>0?text:"Enter Something to preview it here"}</p>
+        <p>{text1}</p>
+        <textarea className="form-control" value={text1} onChange={handleOnChange} id="myBox" rows="14"></textarea>
+        <button className="btn btn-pimary" onClick={handleUpClick}> Convert to upper case</button>
     </div>
     </>
   )
